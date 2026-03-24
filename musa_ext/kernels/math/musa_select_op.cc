e94c3ed2aa23528f262c523bef8593b8ee2a6160
awexxxx <106058408+awexxxx@users.noreply.github.com>
2026-03-24 10:49:16 +0800

    feat: Optimize Select op (#115)
    * feat: Optimize Select op
    
    ---------
    
    Co-authored-by: hxc <>

--- musa_ext/kernels/math/musa_select_op.cc
+++ musa_ext/kernels/math/musa_select_op.cc
@@ -74,7 +74,15 @@ class MusaSelectOp : public MusaOpKernel {
     }
 
     Tensor* output = nullptr;
-    OP_REQUIRES_OK(ctx, ctx->allocate_output(0, output_shape, &output));
+    // ==: 合并output&&input
+    if (then_t.shape() == output_shape) {
+      const std::vector<int> forwardable_input_indices = {1};
+      OP_REQUIRES_OK(
+          ctx, ctx->forward_input_or_allocate_output(
+                   forwardable_input_indices, 0, output_shape, &output));
+    } else {
+      OP_REQUIRES_OK(ctx, ctx->allocate_output(0, output_shape, &output));
+    }
     if (output->NumElements() == 0) return;
 
     MusaDevice* device = reinterpret_cast<MusaDevice*>(ctx->device());
