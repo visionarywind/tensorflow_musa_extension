3f5a4bac782e5e5bf4411529bd469992733b9483
VisionaryWind <visionarywind@icloud.com>
2026-04-23 12:23:49 +0800

    1

--- musa_ext/kernels/math/musa_addn_op.cc
+++ musa_ext/kernels/math/musa_addn_op.cc
@@ -117,8 +117,90 @@ inline mStatus MusaZeroMemoryAsync(void* ptr, size_t size,
   std::vector<char> zeros(size, 0);
   // MusaMemcpyAsyncH2D is confirmed to return mStatus by the compiler error
   // context
-  return MusaMemcpyAsyncH2D(ptr, zeros.data(), size, stream);
+  auto ret = MusaMemcpyAsyncH2D(ptr, zeros.data(), size, stream);
+  musaStreamSynchronize(stream);
+  return ret;
 }
+
+namespace {
+void DumpMusaTensorToHost(OpKernelContext* ctx, const Tensor& device_tensor,
+                          const string& name) {
+  if (device_tensor.NumElements() == 0) {
+    LOG(ERROR) << std::this_thread::get_id() << "[Dump] " << name
+               << " | Empty Tensor | Shape: "
+               << device_tensor.shape().DebugString();
+    return;
+  }
+
+  std::stringstream ss;
+  const DataType dtype = device_tensor.dtype();
+  ss << std::this_thread::get_id()
+     << "=================================================="
+     << "[Dump] " << name << " | Type: " << DataTypeString(dtype)
+     << " | Shape: " << device_tensor.shape().DebugString()
+     << " | Device Addr: " << device_tensor.data();
+
+  Tensor host_tensor;
+  AllocatorAttributes cpu_alloc;
+  cpu_alloc.set_on_host(true);
+  OP_REQUIRES_OK(ctx, ctx->allocate_temp(dtype, device_tensor.shape(),
+                                         &host_tensor, cpu_alloc));
+
+  musaStream_t stream = GetMusaStreamByCtx(ctx);
+  musaError_t err = musaMemcpyAsync(host_tensor.data(), device_tensor.data(),
+                                    device_tensor.TotalBytes(),
+                                    musaMemcpyDeviceToHost, stream);
+  musaStreamSynchronize(stream);
+  OP_REQUIRES(
+      ctx, err == musaSuccess,
+      errors::Internal("Dump musaMemcpy failed: ", musaGetErrorString(err)));
+
+  const int64_t num_elems = host_tensor.NumElements();
+
+  ss << std::this_thread::get_id() << "\n\tData:";
+  switch (dtype) {
+    case DT_INT32: {
+      int32 mn = INT_MAX, mx = INT_MIN;
+      const int32* data = host_tensor.flat<int32>().data();
+      for (int64_t i = 0; i < num_elems; ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+        ss << data[i] << "\t";
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    case DT_INT64: {
+      int64_t mn = INT64_MAX, mx = INT64_MIN;
+      const int64* data = host_tensor.flat<int64>().data();
+      for (int64_t i = 0; i < num_elems; ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+        ss << data[i] << "\t";
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    case DT_FLOAT: {
+      float mn = FLT_MAX;     // 最大正数
+      float mx = FLT_MIN;
+      const float* data = host_tensor.flat<float>().data();
+      for (int64_t i = 0; i < std::max((int64_t)100, num_elems); ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    default: {
+      LOG(ERROR) << "Unsupported dtype: " << DataTypeString(dtype);
+      return;
+    }
+  }
+
+  LOG(ERROR) << ss.str();
+}
+}  // namespace 
 
 // Common implementation for AddN Compute
 template <typename T>
@@ -147,6 +229,11 @@ void AddNCompute(OpKernelContext* ctx, mFormat format,
   if (num_inputs == 1) {
     ctx->set_output(0, ctx->input(0));
     return;
+  }
+
+  for (int i = 0; i < num_inputs; ++i) {
+    const Tensor& input = ctx->input(i);
+    DumpHostTensorToHost(ctx, input, "Input " + std::to_string(i));
   }
 
   // ==========================================================================
