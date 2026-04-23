379d667d408770a87599a15b825014998d316ae2
VisionaryWind <visionarywind@icloud.com>
2026-04-23 19:16:31 +0800

    1

--- musa_ext/kernels/array/musa_reshape_op.cc
+++ musa_ext/kernels/array/musa_reshape_op.cc
@@ -14,6 +14,87 @@
 namespace tensorflow {
 namespace musa {
 
+
+namespace {
+void DumpMusaTensorToHost(OpKernelContext* ctx, const Tensor& device_tensor,
+                          const string& name) {
+  if (device_tensor.NumElements() == 0) {
+    LOG(ERROR) << std::this_thread::get_id() << "[Dump] " << name
+               << " | Empty Tensor | Shape: "
+               << device_tensor.shape().DebugString();
+    return;
+  }
+
+  std::stringstream ss;
+  const DataType dtype = device_tensor.dtype();
+  ss << std::this_thread::get_id()
+     << "=================================================="
+     << "[Dump] " << name << " | Type: " << DataTypeString(dtype)
+     << " | Shape: " << device_tensor.shape().DebugString()
+     << " | Device Addr: " << device_tensor.data();
+
+  Tensor host_tensor;
+  AllocatorAttributes cpu_alloc;
+  cpu_alloc.set_on_host(true);
+  OP_REQUIRES_OK(ctx, ctx->allocate_temp(dtype, device_tensor.shape(),
+                                         &host_tensor, cpu_alloc));
+
+  musaStream_t stream = GetMusaStreamByCtx(ctx);
+  musaError_t err = musaMemcpyAsync(host_tensor.data(), device_tensor.data(),
+                                    device_tensor.TotalBytes(),
+                                    musaMemcpyDeviceToHost, stream);
+  musaStreamSynchronize(stream);
+  OP_REQUIRES(
+      ctx, err == musaSuccess,
+      errors::Internal("Dump musaMemcpy failed: ", musaGetErrorString(err)));
+
+  const int64_t num_elems = host_tensor.NumElements();
+
+  ss << std::this_thread::get_id() << "\n\tData:";
+  switch (dtype) {
+    case DT_INT32: {
+      int32 mn = INT_MAX, mx = INT_MIN;
+      const int32* data = host_tensor.flat<int32>().data();
+      for (int64_t i = 0; i < num_elems; ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+        ss << data[i] << "\t";
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    case DT_INT64: {
+      int64_t mn = INT64_MAX, mx = INT64_MIN;
+      const int64* data = host_tensor.flat<int64>().data();
+      for (int64_t i = 0; i < num_elems; ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+        ss << data[i] << "\t";
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    case DT_FLOAT: {
+      float mn = FLT_MAX;     // 最大正数
+      float mx = FLT_MIN;
+      const float* data = host_tensor.flat<float>().data();
+      for (int64_t i = 0; i < std::max((int64_t)100, num_elems); ++i) {
+        mn = std::min(mn, data[i]);
+        mx = std::max(mx, data[i]);
+      }
+      ss << "min - " << mn << ", max - " << mx << "\t";
+      break;
+    }
+    default: {
+      LOG(ERROR) << "Unsupported dtype: " << DataTypeString(dtype);
+      return;
+    }
+  }
+
+  LOG(ERROR) << ss.str();
+}
+}  // namespace 
+
 template <typename T>
 class MusaReshapeOp : public MusaOpKernel {
  public:
@@ -33,6 +114,8 @@ class MusaReshapeOp : public MusaOpKernel {
 
     const Tensor& input = ctx->input(0);
     const Tensor& sizes = ctx->input(1);
+
+    DumpMusaTensorToHost(ctx, input, "input");
 
     TensorShape shape;
     int64 unknown_index = -1;
